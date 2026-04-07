3562530af14b905e32d2abc13d210a711d995f90
angst-pog <kevin.uriel.che@gmail.com>
2026-04-06 19:06:13 -0600

    init

---
+++ src/Application2XXE00/Figura2XXE00.cs
@@ -0,0 +1,22 @@
+﻿// Archivo: ./src/Application2XXE00/Figura2XXE00.cs
+public class Figura2XXE00 
+{
+    // Método estático para calcular el Perímetro
+    public static double Perimetro(double ladoA , double ladoB) 
+    {
+         return 2*(ladoA+ladoB);
+    }
+
+    // Método estático para calcular el Área
+    public static double Area(double ladoB , double ladoC) 
+    {
+         return ladoB*ladoC;
+    }
+
+    //Metodo estatico para calcular el volumen
+
+    public static double Volumen(double ladoA, double ladoB, double ladoC)
+     {
+          return ladoA*ladoB*ladoC;
+     }
+}
---
+++ src/ConsoleApp2XXE00/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Declaración de variables
+double ladoA = 5;
+double ladoB = 10;
+double ladoC = 15;
+
+// 2. Consumo de la Librería de Clases (Invocación de métodos estáticos)
+double perimetro = Figura2XXE00.Perimetro(ladoA,ladoB);
+double area = Figura2XXE00.Area(ladoB,ladoC);
+double volumen = Figura2XXE00.Volumen(ladoA,ladoB,ladoC);
+
+// 3. Salida en Consola
+Console.WriteLine($"El perímetro de un Paralelogramo de {ladoA} y de {ladoB}cm es {perimetro}cm");
+Console.WriteLine($"El área de un Paralelogramo con una base de {ladoB}cm y una altura de{ladoC} es {area}cm2");
+Console.WriteLine($"El volumen de un Paralelogramo con dimenciones de {ladoA},{ladoB},{ladoC}cm2 es de {volumen}cm3");
